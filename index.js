edc6bbcc3733d3626efa896acf0a684eba40bb83
Evan LaRusso <lalalarusso@gmail.com>
2019-04-01 03:54:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ function logShout(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase()) return "I can't hear you!";
-  else if (string === "I love you, Grandma.") return "I love you, too.";
-  else if (string === string.toUpperCase()) return "YES INDEED!";
+  if (string === string.toLowerCase()) return "I can't hear you!"
 }
   
 /*
